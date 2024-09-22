d498d06255ed7c94788d5ef9f59b7ec67912f2ce
ClariVM <claribelmerlo@hotmail.com>
2024-09-22 09:09:35 -0300

    section normativas was created

--- FrontEnd/src/components/contact/Contact.jsx
+++ FrontEnd/src/components/contact/Contact.jsx
@@ -6,62 +6,6 @@ import "./contact.css";
 function Contact() {
   return (
     <>
-<<<<<<< HEAD
-      <Form className="form">
-        <h3>Contacto</h3>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email</Form.Label>
-          <Form.Control size="md" type="email" placeholder="mail@ejemplo.com" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control
-            size="md"
-            type="text"
-            placeholder="Nombre y Apellido."
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Mensaje</Form.Label>
-          <Form.Control
-            size="md"
-            as="textarea"
-            rows={3}
-            placeholder="Escriba su mensaje..."
-          />
-        </Form.Group>
-
-        <Button>Enviar</Button>
-      </Form>
-
-      <Form className="form">
-        <h3>Contacto</h3>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email</Form.Label>
-          <Form.Control size="md" type="email" placeholder="mail@ejemplo.com" />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control
-            size="md"
-            type="text"
-            placeholder="Nombre y Apellido."
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Mensaje</Form.Label>
-          <Form.Control
-            size="md"
-            as="textarea"
-            rows={3}
-            placeholder="Escriba su mensaje..."
-          />
-        </Form.Group>
-=======
       <div className='container-forms'>
 
           <Form className='form'>
@@ -100,7 +44,6 @@ function Contact() {
               <Form.Label>Mensaje</Form.Label>
               <Form.Control size='md' as="textarea" rows={3} placeholder='Escriba su mensaje...'/>
             </Form.Group>
->>>>>>> 1eb4bce311f1166558d3d3d5bbe5da1ca066c048
 
             <Button>Enviar</Button>
           </Form>
--- FrontEnd/src/components/searchFilter/SearchFilter.jsx
+++ FrontEnd/src/components/searchFilter/SearchFilter.jsx
@@ -1,23 +1,47 @@
 import Form from "react-bootstrap/Form";
-import "./searchFilter.css"
+import "./searchFilter.css";
 
 function SearchFilter() {
   return (
-    <>
+    
       <div className="filter-container">
         <Form className="filter">
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <h1>Búsqueda de normativas </h1>
-            <Form.Label>Ejemplo</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
+          <h1>BÚSQUEDA DE NORMATIVAS</h1>
+
+          <div className="inputs-filter">
+            <div>
+            <Form.Label>Por tipo de norma</Form.Label>
+            <Form.Select aria-label="Default select example">
+              <option>---selecciona una---</option>
+              <option value="Ordenanza">Ordenanza</option>
+              <option value="Minuta">Minuta</option>
+              <option value="Declaracion">Declaración</option>
+              <option value="Resolucion">Resolución</option>
+              <option value="Decreto">Decreto</option>
+            </Form.Select>
+            </div>
+            <div>
+            <Form.Group>
+              <Form.Label>Por texto</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            </div>
+            <div>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            </div>
+            
+          </div>
+          <div className="btn-container">
+          <button className="btn-form">Limpiar</button>
+          <button className="btn-form">Buscar</button>
+        </div>
         </Form>
       </div>
-    </>
   );
 }
 
