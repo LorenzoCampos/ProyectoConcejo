b845046fbe0f35c8bc26c800ac83e144ec3fb9f8
CannataJulian <cannatajulian@gmail.com>
2024-09-30 16:03:36 -0300

    Login was created

--- FrontEnd/src/components/login/Login.jsx
+++ FrontEnd/src/components/login/Login.jsx
@@ -1,11 +1,32 @@
-import "./login.css"
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
 
+import "./login.css";
 
 function Login() {
-    return(
-        <div className="login-container">
-            <h1>hola</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="login-container">
+        <h2>Iniciar Sesión</h2>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-3"
+        >
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingPassword"
+          label="Password"
+          className="mb-3"
+        >
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+        <Button variant="primary" type="submit">Iniciar Sesión</Button>{' '}
+      </div>
+    </div>
+  );
 }
 export default Login;
--- FrontEnd/src/components/pages/Access.jsx
+++ FrontEnd/src/components/pages/Access.jsx
@@ -1,14 +1,12 @@
 
 import Login from "../login/Login";
-import Social from "../social/Social";
+
 
 
 function Access() {
     return(
         <>
-
         <Login/>
-        <Social />
         </>
     )
 }
