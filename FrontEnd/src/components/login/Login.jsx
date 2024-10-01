b9f385a084a2f899f1493bf50762c3d8ff52e2a0
CannataJulian <cannatajulian@gmail.com>
2024-10-01 17:56:48 -0300

    login with credentials was created

--- FrontEnd/src/components/login/Login.jsx
+++ FrontEnd/src/components/login/Login.jsx
@@ -1,50 +1,86 @@
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import "./login.css";
-import {  useState } from "react";
-const endpoint = 'http://lkfc51ph-8000.brs.devtunnels.ms/api'
 
+function getDeviceId() {
+  let deviceId = localStorage.getItem("deviceId");
 
+  if (!deviceId) {
+    deviceId = "device_" + Math.random().toString(36).substr(2, 9);
+    localStorage.setItem("deviceId", deviceId);
+  }
+  console.log(deviceId);
+
+  return deviceId;
+}
 function Login() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
- 
 
-  const loginData = async (e)=>{
+  const loginData = async (e) => {
     e.preventDefault();
-  
+
     try {
-      const response = await axios.post(`${endpoint}/login`, {
+      const deviceId = getDeviceId();
+
+      let headersList = {
+        "Device-Id": deviceId,
+        "Content-Type": "application/json",
+      };
+
+      let bodyContent = JSON.stringify({
         email: email,
         password: password,
       });
-   
-      console.log(response.data);
-    
 
-    const token = response.data.token;
-    console.log(token);
+      let reqOptions = {
+        url: "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/login",
+        method: "POST",
+        headers: headersList,
+        data: bodyContent,
+      };
 
-    // Almacena el token en el almacenamiento local
-    localStorage.setItem("authToken", token);
+      const response = await axios.request(reqOptions);
 
+      // Obtiene el token de la respuesta
+      const token = response.data.token;
 
-    if(response.status === 200){
-      navigate("/Home");
+      // Almacena el token en el almacenamiento local
+      localStorage.setItem("authToken", token);
+
+      // console.log(token);
+
+      if (response.status === 200) {
+        navigate("/"); // Home
+      }
+    } catch (error) {
+      if (error.response) {
+        // El servidor respondió con un estado diferente a 2xx
+        console.error("Datos de la respuesta de error:", error.response.data);
+        console.error(
+          "Estado de la respuesta de error:",
+          error.response.status
+        );
+        console.error(
+          "Encabezados de la respuesta de error:",
+          error.response.headers
+        );
+      } else if (error.request) {
+        // La solicitud fue hecha pero no se recibió respuesta
+        console.error("Solicitud de error:", error.request);
+      } else {
+        // Algo sucedió al configurar la solicitud
+        console.error("Mensaje de error:", error.message);
+      }
     }
-      
-    console.log(response.data); 
-    }catch(error){
-
-      console.error(error);
-    }
-  }
+  };
 
   return (
     <div className="container">
@@ -56,27 +92,33 @@ function Login() {
             label="Email"
             className="mb-3"
           >
-            <Form.Control type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            required placeholder="name@example.com" />
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+              placeholder="name@example.com"
+            />
           </FloatingLabel>
-
           <FloatingLabel
             controlId="floatingPassword"
             label="Contraseña"
             className="mb-3"
           >
-          <Form.Control type="password" placeholder="Password" value={password}
-                  onChange={(event) => setPassword(event.target.value)}
-                  required/>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
           </FloatingLabel>
-        <Button  variant="primary" type="submit">Iniciar Sesión</Button>{' '}
+          <Button variant="primary" type="submit">
+            Iniciar Sesión
+          </Button>{" "}
         </Form>
-        
       </div>
     </div>
-    
   );
 }
 export default Login;
