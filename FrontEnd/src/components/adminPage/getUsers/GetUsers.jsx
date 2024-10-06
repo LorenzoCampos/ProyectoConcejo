f93f094bf1c5d76e3db43718d748dcec010a89f2
CannataJulian <cannatajulian@gmail.com>
2024-10-06 08:37:35 -0300

    minor changes

--- FrontEnd/src/components/adminPage/getUsers/GetUsers.jsx
+++ FrontEnd/src/components/adminPage/getUsers/GetUsers.jsx
@@ -1,26 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 
-import axios from "axios";
-const API =
-  "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/login";
-const API_ROLE =
-  "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/user/role";
-
-function GetUsers() {
-  const [data, setData] = useState([]);
-  const [role, setRole] = useState("");
-
-  useEffect(() => {
-    getAlldata();
-  });
-=======
 import axios from "axios";
 import "./getUsers.css";
 
-const api = "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/user/all";
+const API =
+  "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/user/all";
 
 function GetUsers() {
   const [data, setData] = useState([]);
@@ -29,7 +15,6 @@ function GetUsers() {
   useEffect(() => {
     getAlldata();
   }, []);
->>>>>>> 623463be548ea2b3912556553489eeada96b9eae
 
   const getAlldata = async () => {
     try {
@@ -38,15 +23,8 @@ function GetUsers() {
         "Content-Type": "application/json",
       };
 
-      /*       let bodyContent = JSON.stringify({
-      }); */
-
       let reqOptions = {
-<<<<<<< HEAD
         url: `${API}`,
-=======
-        url: api,
->>>>>>> 623463be548ea2b3912556553489eeada96b9eae
         method: "GET",
         headers: headersList,
       };
@@ -56,99 +34,7 @@ function GetUsers() {
     } catch (error) {
       console.error("Error al obtener los usuarios:", error);
     }
-<<<<<<< HEAD
 
-    const editRole = async (e) => {
-      e.preventDefault();
-      try {
-        let headersList = {
-          Authorization: "Bearer " + localStorage.getItem("authToken"),
-          "Content-Type": "application/json",
-        };
-
-        let bodyContent = JSON.stringify({
-          role: role,
-        });
-
-        let reqOptions = {
-          url: `${API_ROLE}`,
-          method: "POST",
-          headers: headersList,
-          data: bodyContent,
-        };
-
-        const response = await axios.request(reqOptions);
-
-        if (response.status === 200) {
-          getAlldata();
-          console.log("Rol cambiado exitosamente:", response.data);
-
-          /* Funcion para deslogear a los usuarios con su rol cambiado */
-
-          /* if (response.data.role === "admin") {
-            localStorage.removeItem("authToken");
-            window.location.reload();
-          } */
-        } else {
-          console.error("Error al cambiar el rol:", response.data);
-        }
-      } catch (error) {
-        console.error("Error al cambiar el rol:", error);
-      }
-    };
-
-    return (
-      <div className="container">
-        <div className="datas-container">
-          <h1>Lista de usuarios</h1>
-          <table>
-            <th>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Acciones</th>
-              </tr>
-            </th>
-
-            <th>
-              {data.map((data) => (
-                <tr key={data.id}>
-                  <td>{data.name}</td>
-                  <td>{data.email}</td>
-                  <td>{data.role}</td>
-                  <td>
-                    <Form onSubmit={editRole}>
-                      <Form.Select
-                        aria-label="Default select example"
-                        onChange={(event) => setRole(event.target.value)}
-                      >
-                        <div>
-                          <option> ---Cambiar Rol--- </option>
-                          <option value="admin">Administrador</option>
-                          <option value="secretario">Secretario</option>
-                          <option value="concejal">Concejal</option>
-                          <option value="cm">CM</option>
-                          <option value="user">Usuario</option>
-                        </div>
-                      </Form.Select>
-                      <td>
-                        <Button variant="primary" type="submit">
-                          Editar Rol
-                        </Button>
-                      </td>
-                    </Form>
-                  </td>
-                </tr>
-              ))}
-            </th>
-          </table>
-        </div>
-      </div>
-    );
-=======
->>>>>>> 623463be548ea2b3912556553489eeada96b9eae
   };
 
   const handleRoleChange = (e, userId) => {
@@ -196,7 +82,6 @@ function GetUsers() {
       <table className="table">
         <thead>
           <tr>
-            <th>ID</th>
             <th>Nombre</th>
             <th>Roles</th>
             <th>Acciones</th>
@@ -205,7 +90,6 @@ function GetUsers() {
         <tbody>
           {data.map((user) => (
             <tr key={user.id}>
-              <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.role ? user.role : "Sin rol"}</td>
               <td>
@@ -216,10 +100,10 @@ function GetUsers() {
                     onChange={(e) => handleRoleChange(e, user.id)}
                   >
                     <option value="">---Cambiar Rol---</option>
-                    <option value="User">User</option>
-                    <option value="Secretario">Secretario</option>
-                    <option value="Concejal">Concejal</option>
-                    <option value="cm">Redes</option>
+                    <option value="secretario">Secretario</option>
+                    <option value="concejal">Concejal</option>
+                    <option value="cm">CM</option>
+                    <option value="user">Usuario</option>
                   </Form.Control>
                 </Form.Group>
                 <Button variant="primary" onClick={() => updateRole(user.id)}>
