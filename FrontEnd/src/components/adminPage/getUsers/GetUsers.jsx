fa7286ea1667990bd1092fbec26c817db3881dae
CannataJulian <cannatajulian@gmail.com>
2024-10-06 08:18:11 -0300

    const editRole was created

--- FrontEnd/src/components/adminPage/getUsers/GetUsers.jsx
+++ FrontEnd/src/components/adminPage/getUsers/GetUsers.jsx
@@ -1,35 +1,81 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import{useState, useEffect} from  "react";
+import { useState, useEffect } from "react";
 
 import axios from "axios";
-const api = "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/login";
+const API =
+  "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/login";
+const API_ROLE =
+  "https://lkfc51ph-443.brs.devtunnels.ms/ProyectoConcejo/backend-api/public/api/user/role";
 
-function Getdata() {
-  const [data, setdata] = useState([]);
+function GetUsers() {
+  const [data, setData] = useState([]);
+  const [role, setRole] = useState("");
 
   useEffect(() => {
     getAlldata();
-  }, );
+  });
 
   const getAlldata = async () => {
     try {
       let headersList = {
-        "Authorization": "Bearer " + localStorage.getItem("authToken"),
+        Authorization: "Bearer " + localStorage.getItem("authToken"),
         "Content-Type": "application/json",
       };
 
+      /*       let bodyContent = JSON.stringify({
+      }); */
+
       let reqOptions = {
-        url: `${api}`,
+        url: `${API}`,
         method: "GET",
         headers: headersList,
       };
 
       const response = await axios.request(reqOptions);
-      setdata(response.data);
+      setData(response.data);
     } catch (error) {
       console.error("Error al obtener los usuarios:", error);
     }
+
+    const editRole = async (e) => {
+      e.preventDefault();
+      try {
+        let headersList = {
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+          "Content-Type": "application/json",
+        };
+
+        let bodyContent = JSON.stringify({
+          role: role,
+        });
+
+        let reqOptions = {
+          url: `${API_ROLE}`,
+          method: "POST",
+          headers: headersList,
+          data: bodyContent,
+        };
+
+        const response = await axios.request(reqOptions);
+
+        if (response.status === 200) {
+          getAlldata();
+          console.log("Rol cambiado exitosamente:", response.data);
+
+          /* Funcion para deslogear a los usuarios con su rol cambiado */
+
+          /* if (response.data.role === "admin") {
+            localStorage.removeItem("authToken");
+            window.location.reload();
+          } */
+        } else {
+          console.error("Error al cambiar el rol:", response.data);
+        }
+      } catch (error) {
+        console.error("Error al cambiar el rol:", error);
+      }
+    };
 
     return (
       <div className="container">
@@ -53,14 +99,25 @@ function Getdata() {
                   <td>{data.email}</td>
                   <td>{data.role}</td>
                   <td>
-                    <Form>
-                      <div>
-                        <option value="---Cambiar Rol---"></option>
-                        <option value="Admin">Admin</option>
-                        <option value="User">User</option>...
-                      </div>
-                      
-                      <Button  variant="primary">Editar Rol</Button>
+                    <Form onSubmit={editRole}>
+                      <Form.Select
+                        aria-label="Default select example"
+                        onChange={(event) => setRole(event.target.value)}
+                      >
+                        <div>
+                          <option> ---Cambiar Rol--- </option>
+                          <option value="admin">Administrador</option>
+                          <option value="secretario">Secretario</option>
+                          <option value="concejal">Concejal</option>
+                          <option value="cm">CM</option>
+                          <option value="user">Usuario</option>
+                        </div>
+                      </Form.Select>
+                      <td>
+                        <Button variant="primary" type="submit">
+                          Editar Rol
+                        </Button>
+                      </td>
                     </Form>
                   </td>
                 </tr>
@@ -73,4 +130,4 @@ function Getdata() {
   };
 }
 
-export default Getdata;
+export default GetUsers;
