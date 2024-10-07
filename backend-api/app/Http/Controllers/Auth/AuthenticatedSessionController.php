a11ce95fa71d96ba7ab0a481a9e97dc410e242bf
LoloCampos <lolocampos544@gmail.com>
2024-10-07 15:34:13 -0300

    Minor corrections.

--- backend-api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend-api/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Auth\AuthenticationException;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -28,14 +24,14 @@ class AuthenticatedSessionController extends Controller
             if (!$user) {
                 return response()->json([
                     'message' => 'El correo electrónico no está registrado.'
-                ], 401);
+                ], 422);
             }
 
             // Intentar autenticar al usuario
             if (!Hash::check($request->input('password'), $user->password)) {
                 return response()->json([
                     'message' => 'La contraseña es incorrecta.'
-                ], 401);
+                ], 422);
             }
 
             // Autenticar al usuario
@@ -71,55 +67,6 @@ class AuthenticatedSessionController extends Controller
             ], 500);
         }
     }
-
-
-
-    /* public function store(LoginRequest $request): JsonResponse
-    {
-        try {
-            try {
-                // Aunteticar el usuario
-                $request->authenticate();
-            } catch (\Exception $e) {
-                return response()->json([
-                    'message' => $e->getMessage()
-                ], 401);
-            }
-
-            $user = $request->user();
-
-            // Crear un nuevo token para el usuario
-            $deviceId = $request->header('Device-Id');
-
-            // Eliminar el token existente
-            if ($user->tokens()->where('name', $deviceId)->exists()) {
-                $user->tokens()->where('name', $deviceId)->delete();
-            }
-
-            // Crear el nuevo token
-            $token = $user->createToken($deviceId);
-
-            $role = $user->roles->pluck('name');
-
-            $role = $role[0];
-
-            // Devolver el token
-            return response()->json([
-                "user" => [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'email' => $user->email
-                ],
-                "token" => $token->plainTextToken,
-                "role" => $role
-            ], 200);
-        } catch (\Exception $e) {
-            // Manejar el error
-            return response()->json([
-                'message' => $e->getMessage()
-            ], 400);
-        }
-    } */
 
     /**
      * Destroy an authenticated session.
