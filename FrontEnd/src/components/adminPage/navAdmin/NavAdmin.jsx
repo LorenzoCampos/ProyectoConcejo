3638f496806f698d6484b37c91fb77fd2136398e
CannataJulian <cannatajulian@gmail.com>
2024-10-08 18:20:50 -0300

    minor changes

--- FrontEnd/src/components/adminPage/navAdmin/NavAdmin.jsx
+++ FrontEnd/src/components/adminPage/navAdmin/NavAdmin.jsx
@@ -1,61 +1,59 @@
-import Container from 'react-bootstrap/Container';
-import { useEffect, useState } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
-
-
+import Container from "react-bootstrap/Container";
+import { useEffect, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 import "./navAdmin.css";
-import Register from '../register/Register';
-import Logout from '../../logout/Logout';
+
+import Register from "../register/Register";
+import Logout from "../../logout/Logout";
 
 function NavAdmin() {
-  const [name, setName] = useState('');
- const [showRegister, setShowRegister] = useState(false);
+  const [name, setName] = useState("");
+  const [showRegister, setShowRegister] = useState(false);
 
+  useEffect(() => {
+    const storedName = localStorage.getItem("userName");
+    if (storedName) {
+      setName(storedName);
+    }
+  }, []);
 
-  
-    useEffect(() => {
-        const storedName = localStorage.getItem("userName");
-        if (storedName) {
-          setName(storedName);
-        }
-      }, []);
-
-
-      const handleRegisterClick = () => {
-        setShowRegister(true);  
-    };
+  const handleRegisterClick = () => {
+    setShowRegister(true);
+  };
   return (
     <>
-    <div className="nav-admin">
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-      <Navbar.Brand as={Link} to="/"><img className="logo" src="public/logo1.png" alt="Logo" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <NavDropdown title={name || "Usuario"} id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={handleRegisterClick}>Registrar un nuevo usuario</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-              <Logout />
-              </NavDropdown.Item>
-            </NavDropdown>
-         
-        </Navbar.Collapse>
-      </Container>
-      
-    </Navbar>
+      <div className="nav-admin">
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              <img className="logo" src="public/logo1.png" alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <NavDropdown title={name || "Usuario"} id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={handleRegisterClick}>
+                  Registrar un nuevo usuario
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  <Logout />
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
 
-    </div>
-        
-    {showRegister && <Register />}
-
+      {showRegister && <Register />}
     </>
   );
 }
