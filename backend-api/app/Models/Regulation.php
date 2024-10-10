8190f53def5ffe5b11a952d5935c7bbb51f377b4
LoloCampos <lolocampos544@gmail.com>
2024-10-10 18:08:34 -0300

    Modifications in regulation from name to type.

--- backend-api/app/Models/Regulation.php
+++ backend-api/app/Models/Regulation.php
@@ -10,7 +10,7 @@ class Regulation extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
+        'type',
         'number',
         'date',
         'state',
--- backend-api/database/migrations/2024_10_04_171720_create_regulations_table.php
+++ backend-api/database/migrations/2024_10_04_171720_create_regulations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('regulations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('type');
             $table->integer('number');
             $table->date('date');
             $table->string('state');
