8f7bd72c5d60da3db4ec4640aa3b8423cc145329
LoloCampos <lolocampos544@gmail.com>
2024-11-06 16:45:02 -0300

    Email verification adapted for api (in process).

--- backend-api/app/Http/Controllers/Auth/VerifyEmailController.php
+++ backend-api/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,53 +6,36 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 
 class VerifyEmailController extends Controller
 {
-    /**
-     * Mark the authenticated user's email address as verified.
-     */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    /*
+        * Mark the authenticated user's email address as verified.
+    */
+    public function __invoke($id, $hash)
     {
-        if ($request->user()->hasVerifiedEmail()) {
+        $user = User::findOrFail($id); // Busca el usuario por id en la ruta
+
+        // Verifica si el hash en la URL es válido para este usuario.
+        if (! hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
             return redirect()->intended(
-                config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+                env('FRONTEND_URL'). '?invalid-link'
             );
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->intended(
+                // ruta proveniente del .env
+                env('FRONTEND_URL') . '?verified=1'
+            );
+        }
+
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
         }
 
         return redirect()->intended(
-            config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+            env('FRONTEND_URL'). '?verified=1'
         );
-    }
-
-    public function verify(Request $request){
-
-        // Validar que el enlace es válido y tiene la firma correcta
-        if (!$request->hasValidSignature()) {
-            return response()->json(['message' => 'Invalid verification link'], 403);
-        }
-
-        // Buscar al usuario basado en el ID proporcionado en la URL
-        $user = User::findOrFail($request->route('id'));
-
-        // Verificar si el correo ya fue verificado
-        if ($user->hasVerifiedEmail()) {
-            return response()->json(['message' => 'Email already verified'], 400);
-        }
-
-        // Si el correo no ha sido verificado, lo marcamos como verificado
-        if ($user->markEmailAsVerified()) {
-            event(new Verified($user)); // Despachar el evento de verificación
-        }
-
-        return response()->json(['message' => 'Email successfully verified'], 200);
-
     }
 }
