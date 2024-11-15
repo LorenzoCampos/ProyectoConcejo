4299ccc8e3d2f96b7034d98527337a4ae7b196ec
CannataJulian <cannatajulian@gmail.com>
2024-11-15 17:01:57 -0300

    minor changes

--- FrontEnd/src/components/news/SeeNew.jsx
+++ FrontEnd/src/components/news/SeeNew.jsx
@@ -5,7 +5,7 @@ function SeeNew({ news }) {
   return (
     <div className="container-seeNew">
       <div className="container-photo">
-        <img src={news.image} alt="" />
+        <img src={news.image} alt="" width={"80%"} objectFit={"cover"}/>
       </div>
       <div className="container-allNew">
         <h2 className="title-new">{news.title}</h2>
