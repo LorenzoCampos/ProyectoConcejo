f7620521c8334e68ab816108c1b4ae51f6e29a81
LoloCampos <lolocampos544@gmail.com>
2024-12-07 16:33:36 -0300

    Minor correction in migration of the authors table.

--- backend-api/database/migrations/2024_10_04_172242_create_authors_table.php
+++ backend-api/database/migrations/2024_10_04_172242_create_authors_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->foreignId('fk_regulation')->constrained('regulations'); // Relación con regulations
-            $table->string('type');
             $table->string('name')->nullable();
             $table->timestamps();
         });
