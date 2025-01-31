128244d35a4de8093f2159a21b1200f0d9b2615b
LoloCampos <lolocampos544@gmail.com>
2025-01-31 15:38:21 -0300

    Correction Vite and .gitignore.

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import PurgeCSSPlugin from 'vite-plugin-purgecss';
-import glob from 'glob-all';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    PurgeCSSPlugin({
-      content: glob.sync([
-        './index.html',
-        './src/**/*.{js,jsx,ts,tsx,vue}',  // Ajusta las rutas de tus archivos de código
-      ]),
-    }),
-  ],
-  define: {
-    global: 'window',
-  },
-});
+  plugins: [react()],
+})
