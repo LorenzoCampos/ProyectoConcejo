e2a69ef5427454cbd526c6fa8a6b2b299bb314ac
LoloCampos <lolocampos544@gmail.com>
2025-02-06 03:22:44 -0300

    Small styling correction in the nav bar.

--- FrontEnd/src/components/navbar/NavbarForRole.jsx
+++ FrontEnd/src/components/navbar/NavbarForRole.jsx
@@ -38,163 +38,145 @@ function NavbarForRole() {
 
   return (
     <>
-      <Navbar key="sm" expand="sm" className="bg-body-tertiary mb-3">
-        <Container fluid>
-          <Navbar.Brand as={Link} to="/">
-            <img className="logo" src="../../public/logo1.png" alt="Logo" />
-          </Navbar.Brand>
+      <div className="nav-cont">
+        <Navbar
+          key="sm"
+          expand="sm"
+          className="bg-body-tertiary mb-3 fixed-top"
+        >
+          <Container fluid>
+            <Navbar.Brand as={Link} to="/">
+              <img className="logo" src="../../public/logo1.png" alt="Logo" />
+            </Navbar.Brand>
 
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-sm`} />
 
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-sm`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
-                <div className="user-info">
-                  {avatar && avatar !== "Usuario" ? (
-                    <img src={avatar} alt="User Avatar" className="avatar" />
-                  ) : (
-                    <FaRegUserCircle className="default-avatar" />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-sm`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-sm`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-sm`}>
+                  <div className="user-info">
+                    {avatar && avatar !== "Usuario" ? (
+                      <img src={avatar} alt="User Avatar" className="avatar" />
+                    ) : (
+                      <FaRegUserCircle className="default-avatar" />
+                    )}
+                    <span className="user-name">{name}</span>
+                  </div>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  {(userRole === "concejal" ||
+                    userRole === "asesor" ||
+                    userRole === "mesa de entrada" ||
+                    userRole === "admin") && (
+                    <>
+                      <Nav.Link
+                        as={Link}
+                        to=""
+                        className="link-nav"
+                        style={{ fontSize: "1rem" }}
+                      >
+                        Ver Normativas
+                      </Nav.Link>
+                      <Nav.Link
+                        as={Link}
+                        to="cargar-normativa"
+                        className="link-nav"
+                        style={{ fontSize: "1rem" }}
+                      >
+                        Cargar normativa
+                      </Nav.Link>
+                    </>
                   )}
-                  <span className="user-name">{name}</span>
-                </div>
-              </Offcanvas.Title>
-            </Offcanvas.Header>
 
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                {(userRole === "concejal" ||
-                  userRole === "asesor" ||
-                  userRole === "mesa de entrada" ||
-                  userRole === "admin") && (
-                  <>
-                    <Nav.Link
-                      as={Link}
-                      to=""
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Ver Normativas
-                    </Nav.Link>
-                    <Nav.Link
-                      as={Link}
-                      to="cargar-normativa"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Cargar normativa
-                    </Nav.Link>
-                  </>
-                )}
+                  {userRole === "admin" && (
+                    <>
+                      <Nav.Link
+                        as={Link}
+                        to="gestionar-usuarios"
+                        className="link-nav"
+                        style={{ fontSize: "1rem" }}
+                      >
+                        Gestionar Usuarios
+                      </Nav.Link>
+                    </>
+                  )}
 
-                {userRole === "admin" && (
-                  <>
-                    <Nav.Link
-                      as={Link}
-                      to="gestionar-usuarios"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Gestionar Usuarios
-                    </Nav.Link>
-                    <Nav.Link
-                      as={Link}
-                      to="registrar-usuario"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Registrar Usuario
-                    </Nav.Link>
-                  </>
-                )}
+                  {userRole === "cm" && (
+                    <>
+                      <Nav.Link
+                        as={Link}
+                        to=""
+                        className="link-nav"
+                        style={{ fontSize: "1rem" }}
+                      >
+                        Gestionar Banners
+                      </Nav.Link>
+                      <Nav.Link
+                        as={Link}
+                        to="ver-noticias"
+                        className="link-nav"
+                        style={{ fontSize: "1rem" }}
+                      >
+                        Gestionar Noticias
+                      </Nav.Link>
+                    </>
+                  )}
 
-                {userRole === "cm" && (
-                  <>
-                    <Nav.Link
-                      as={Link}
-                      to=""
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
+                  <div className="d-none d-sm-block">
+                    <NavDropdown
+                      title={
+                        <div className="user-info">
+                          {avatar && avatar !== "Usuario" ? (
+                            <img
+                              src={avatar}
+                              alt="User Avatar"
+                              className="avatar"
+                            />
+                          ) : (
+                            <>
+                              <FaRegUserCircle className="default-avatar" />
+                              <span className="user-name">{name}</span>
+                            </>
+                          )}
+                        </div>
+                      }
+                      id="user-dropdown"
+                      align="end"
                     >
-                      Gestionar Banners
-                    </Nav.Link>
-                    <Nav.Link
-                      as={Link}
-                      to="ver-noticias"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Gestionar Noticias
-                    </Nav.Link>
-                    <Nav.Link
-                      as={Link}
-                      to="cargar-banner"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Cargar Banner
-                    </Nav.Link>
-                    <Nav.Link
-                      as={Link}
-                      to="cargar-noticia"
-                      className="link-nav"
-                      style={{ fontSize: "1rem" }}
-                    >
-                      Cargar Noticia
-                    </Nav.Link>
-                  </>
-                )}
-
-                <div className="d-none d-sm-block">
-                  <NavDropdown
-                    title={
-                      <div className="user-info">
-                        {avatar && avatar !== "Usuario" ? (
-                          <img
-                            src={avatar}
-                            alt="User Avatar"
-                            className="avatar"
-                          />
-                        ) : (
-                          <>
-                            <FaRegUserCircle className="default-avatar" />
-                            <span className="user-name">{name}</span>
-                          </>
-                        )}
-                      </div>
-                    }
-                    id="user-dropdown"
-                    align="end"
-                  >
-                    {/* <NavDropdown.Item>Ver Perfil</NavDropdown.Item> */}
-                    <NavDropdown.Item as={Link} to="/">
-                      Volver al Home
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item>
-                      <Logout />
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </div>
-                <div className="d-block d-sm-none">
-                  {/* <Nav.Link as={Link} to="/profile">
+                      {/* <NavDropdown.Item>Ver Perfil</NavDropdown.Item> */}
+                      <NavDropdown.Item as={Link} to="/">
+                        Volver al Home
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item>
+                        <Logout />
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </div>
+                  <div className="d-block d-sm-none">
+                    {/* <Nav.Link as={Link} to="/profile">
                     Ver Perfil
                   </Nav.Link> */}
-                  <Nav.Link as={Link} to="/">
-                    Volver al Home
-                  </Nav.Link>
-                  <Nav.Link>
-                    <Logout />
-                  </Nav.Link>
-                </div>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
+                    <Nav.Link as={Link} to="/">
+                      Volver al Home
+                    </Nav.Link>
+                    <Nav.Link>
+                      <Logout />
+                    </Nav.Link>
+                  </div>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </div>
     </>
   );
 }
