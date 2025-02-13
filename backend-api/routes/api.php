75ca846df263b9dcc27e5125b4d6a8e9fd1c436d
LoloCampos <lolocampos544@gmail.com>
2025-02-13 17:59:15 -0300

    Refactor routes.

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -7,27 +7,23 @@ use \App\Http\Controllers\UserController;
 
 Route::prefix('v1')->group(function () {
 
-    Route::middleware(['auth:sanctum'])->group(function () {
-        Route::get('/vllc', function () {
-            return 'Viva La Libertad Carajo';
-        });
+    Route::get('/vllc', function () {
+        return 'Viva La Libertad Carajo';
     });
 
     Route::middleware(['auth:sanctum'])->group(function () {
 
-        Route::middleware(['permission:ver usuarios'])->get('users/non-admin', [UserController::class,'getNonAdminUsers']);
+        Route::middleware(['permission:ver usuarios'])->get('users/non-admin', [UserController::class, 'getNonAdminUsers']);
         Route::middleware(['permission:modificar roles de usuarios'])->patch('users/{user}/role', [UserController::class, 'changeUserRole']);
         Route::middleware(['permission:ver todos los roles'])->get('roles', [UserController::class, 'getAllRoles']);
-
     });
 
     Route::middleware(['auth:sanctum'])->group(function () {
 
-        Route::middleware(['permission:ver normativa'])->get('regulations', [RegulationController::class,'index']);
-        Route::middleware(['permission:ver normativa'])->get('regulations/{id}', [RegulationController::class,'show']);
-        Route::middleware(['permission:crear normativa'])->post('regulations', [RegulationController::class,'store']);
-        Route::middleware(['permission:modificar normativa'])->post('regulations/{id}', [RegulationController::class,'update']);
-
+        Route::middleware(['permission:ver normativa'])->get('regulations', [RegulationController::class, 'index']);
+        Route::middleware(['permission:ver normativa'])->get('regulations/{id}', [RegulationController::class, 'show']);
+        Route::middleware(['permission:crear normativa'])->post('regulations', [RegulationController::class, 'store']);
+        Route::middleware(['permission:modificar normativa'])->post('regulations/{id}', [RegulationController::class, 'update']);
     });
 
     Route::middleware(['auth:sanctum', 'role:cm'])->group(function () {
@@ -42,5 +38,4 @@ Route::prefix('v1')->group(function () {
 
     Route::get('banners/published', [NewsBannerController::class, 'getAllPublishedBanners']);
     Route::get('news/published', [NewsBannerController::class, 'getAllPublishedNews']);
-
 });
