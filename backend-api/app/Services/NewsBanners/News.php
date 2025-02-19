c4ce6abf712012cadec06ed8210c0c283de2ec49
LoloCampos <lolocampos544@gmail.com>
2025-02-18 23:22:45 -0300

    Updated routes for images in banner and news. Url by video for instagram.

--- backend-api/app/Services/NewsBanners/News.php
+++ backend-api/app/Services/NewsBanners/News.php
@@ -10,10 +10,11 @@ class News extends BaseNewBanner
     public function validate(bool $isCreation = true)
     {
         $rules = [
-            'image' => $isCreation ? 'required|image|mimes:jpg,jpeg,png|max:2048' : 'sometimes|image|mimes:jpg,jpeg,png|max:2048',
+            'image' => $isCreation ? 'nullable|image|mimes:jpg,jpeg,png' : 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'video_url' => $isCreation ? 'nullable|url' : 'nullable|url',
             'status' => $isCreation ? 'required|boolean' : 'sometimes|boolean',
-            'title' => $isCreation ? 'required|string|max:510|min:10' : 'sometimes|string|max:510|min:10',
-            'description' => $isCreation ? 'required|string|min:10' : 'sometimes|string|min:10',
+            'title' => $isCreation ? 'required|string|max:510|min:2' : 'sometimes|string|max:510|min:10',
+            'description' => $isCreation ? 'required|string|min:2' : 'sometimes|string|min:10',
             'publication_date' => 'nullable|date',
             'unpublication_date' => 'nullable|date|after_or_equal:publication_date',
         ];
