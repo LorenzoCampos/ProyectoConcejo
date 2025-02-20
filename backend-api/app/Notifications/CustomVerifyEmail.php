2d91d5644b34ce232f2e9248647c4913fc2e04f4
LoloCampos <lolocampos544@gmail.com>
2025-02-20 16:33:01 -0300

    Minor correction in url

--- backend-api/app/Notifications/CustomVerifyEmail.php
+++ backend-api/app/Notifications/CustomVerifyEmail.php
@@ -59,7 +59,7 @@ class CustomVerifyEmail extends Notification
         );
 
         // Define la URL base para el frontend (ajústala según tu entorno)
-        $baseUrl = "https://concejoarroyoseco.vercel.app";
+        $baseUrl = "https://api-concejoarroyoseco.duckdns.org";
 
         // Reemplaza la URL base por la del frontend
         $customerVerificationURL = str_replace(url('/'), $baseUrl, $verificationUrl);
