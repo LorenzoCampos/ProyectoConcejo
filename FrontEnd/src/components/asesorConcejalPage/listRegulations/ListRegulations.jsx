b5063bf25628866c3b269c86e081936a94fa6064
CannataJulian <cannatajulian@gmail.com>
2025-03-10 20:18:04 -0300

    alerta de completar datos creada

--- FrontEnd/src/components/asesorConcejalPage/listRegulations/ListRegulations.jsx
+++ FrontEnd/src/components/asesorConcejalPage/listRegulations/ListRegulations.jsx
@@ -16,9 +16,15 @@ import { FaRegFilePdf } from "react-icons/fa";
 
 import API from "../../../config/apiConfig";
 
+import { Alert } from "react-bootstrap";
+
 import "./listRegulations.css";
 
 function ListRegulations() {
+  const [userName, setUserName] = useState("");
+  const [userLastName, setUserLastName] = useState("");
+  const [emailVerified, setEmailVerified] = useState("");
+
   const [userRole, setUserRole] = useState("");
 
   const [data, setData] = useState([]);
@@ -40,13 +46,33 @@ function ListRegulations() {
 
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const userName = localStorage.getItem("userName");
+    setUserName(userName);
+    const userLastName = localStorage.getItem("userLastName");
+    setUserLastName(userLastName);
+    const emailVerified = localStorage.getItem("email_verified");
+    setEmailVerified(emailVerified);
+  }, []);
+
   // Función para manejar la redirección
   const handleEditClick = (id) => {
-    navigate(`/admin/modificar-normativa/${id}`);
+    if (userRole === "admin") {
+      navigate(`/admin/modificar-normativa/${id}`); // Redirige al CM si el rol es concejal
+    } else if (userRole === "asesor") {
+      navigate(`/asesor-concejal/modificar-normativa/${id}`);
+    } else {
+      setToastMessage("No tienes permiso para realizar esta acción.");
+      setShowErrorToast(true);
+    }
   };
 
   const handleDetailsClick = (id) => {
-    navigate(`/admin/detalles/${id}`);
+    if (userRole === "admin") {
+      navigate(`/admin/detalles/${id}`); // Redirige al CM si el rol es concejal
+    } else {
+      navigate(`/asesor-concejal/detalles/${id}`);
+    }
   };
 
   const getRegulations = async (page = 1, s = {}) => {
@@ -161,10 +187,61 @@ function ListRegulations() {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  const handleClickLinktoProfile = () => {
+    if (userRole === "admin") {
+      navigate("/admin/profile"); // Redirige al CM si el rol es admin
+    } else if (userRole === "cm") {
+      navigate("/cm/profile");
+    } else {
+      navigate("/asesor-concejal/profile");
+    }
+  };
+
+  const handleClickLinktoEmail =  async () => {
+    try {
+      const response = await axios.post(API.EMAIL_VERIFICATION, status, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("authToken"),
+        },
+      });
+
+      if (response.data.status === "verification-link-sent") {
+        setToastMessage("Email de verificación enviado correctamente.");
+        setShowSuccessToast(true);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setToastMessage("Error al enviar el email de verificación.");
+      setShowErrorToast(true);
+    }
+  };
+
   return (
     <div className="page-table">
       <div className="content-page-container">
+        {(userLastName === "null" || !userName === "null" || emailVerified === "false") && (
+          <Alert variant="danger">
+            <h4>Necesitas completar tu perfil</h4>
+            <p>
+              Por favor, completa tu perfil para acceder a todas las
+              funcionalidades de la plataforma.
+            </p>
+            {(userLastName === "null" || !userName === "null") && (
+              <p>
+                - Completa tu perfil {"👉"} {" "}
+                <a onClick={handleClickLinktoProfile} className="link-to-profile">Ir a mi perfil</a>
+              </p>
+            )}
+            {(emailVerified === "false") && (
+              <p>
+                - Verifica tu email {"👉"} {" "}
+                <a onClick={handleClickLinktoEmail} className="link-to-profile">Verificar Mail</a>
+              </p>)}
+          </Alert>
+        )}
         <h1 className="internal-title">Lista de Normativas</h1>
+
         <div className="filter-regulations">
           <div className="filter-inputs">
             <div className="filter-group">
@@ -272,101 +349,103 @@ function ListRegulations() {
                 </tr>
               </thead>
               <tbody>
-              {data.map((regulation) => (
-  <tr key={regulation.id}>
-    <td data-title="Tipo">{translateType(regulation.type)}</td>
-    <td data-title="N°">{regulation.number}</td>
-    <td data-title="Palabras Clave">
-      {regulation.keywords && regulation.keywords.length > 0 ? (
-        regulation.keywords.map((keyword, index) => (
-          <span key={index}>
-            {keyword.word.charAt(0).toUpperCase() + keyword.word.slice(1)}
-            {index < regulation.keywords.length - 1 ? " - " : ""}
-          </span>
-        ))
-      ) : (
-        <span>No keywords available</span>
-      )}
-    </td>
-    <td data-title="Fecha de creación">
-      {formatDate(regulation.created_at)}
-    </td>
-    <td data-title="Estado">
-      {translateState(regulation.state)}
-    </td>
-    <td data-title="Tipo de Autor">
-      {capitalizeFirstLetter(regulation.author_type)}
-    </td>
-    <td data-title="Acciones">
-      <div className="accion-buttons">
-        {/* Solo se renderizan los botones de PDF si el tipo NO es "correspondence" */}
-        {regulation.type !== "correspondence" && (
-          <div>
-            {regulation.pdf_approved && (
-              <a target="_blank" href={regulation.pdf_approved}>
-                <Button
-                  variant="primary"
-                  className="pdf-btn"
-                  title="PDF Aprobado"
-                >
-                  <FaRegFilePdf style={{ color: "white" }} />
-                </Button>
-              </a>
-            )}
-            {regulation.pdf_process ? (
-              <a target="_blank" href={regulation.pdf_process}>
-                <Button
-                  variant="primary"
-                  className="pdf-btn"
-                  title="PDF En Proceso"
-                >
-                  <FaRegFilePdf style={{ color: "white" }} />
-                </Button>
-              </a>
-            ) : (
-              // Botón inactivo para normativas que no tengan PDF
-              <a target="_blank" disabled>
-                <Button
-                  title="PDF no disponible"
-                  variant="secondary"
-                  className="pdf-btn"
-                  disabled
-                >
-                  <FaRegFilePdf style={{ color: "white" }} />
-                </Button>
-              </a>
-            )}
-          </div>
-        )}
+                {data.map((regulation) => (
+                  <tr key={regulation.id}>
+                    <td data-title="Tipo">{translateType(regulation.type)}</td>
+                    <td data-title="N°">{regulation.number}</td>
+                    <td data-title="Palabras Clave">
+                      {regulation.keywords && regulation.keywords.length > 0 ? (
+                        regulation.keywords.map((keyword, index) => (
+                          <span key={index}>
+                            {keyword.word.charAt(0).toUpperCase() +
+                              keyword.word.slice(1)}
+                            {index < regulation.keywords.length - 1
+                              ? " - "
+                              : ""}
+                          </span>
+                        ))
+                      ) : (
+                        <span>No keywords available</span>
+                      )}
+                    </td>
+                    <td data-title="Fecha de creación">
+                      {formatDate(regulation.created_at)}
+                    </td>
+                    <td data-title="Estado">
+                      {translateState(regulation.state)}
+                    </td>
+                    <td data-title="Tipo de Autor">
+                      {capitalizeFirstLetter(regulation.author_type)}
+                    </td>
+                    <td data-title="Acciones">
+                      <div className="accion-buttons">
+                        {/* Solo se renderizan los botones de PDF si el tipo NO es "correspondence" */}
+                        {regulation.type !== "correspondence" && (
+                          <div>
+                            {regulation.pdf_approved && (
+                              <a target="_blank" href={regulation.pdf_approved}>
+                                <Button
+                                  variant="primary"
+                                  className="pdf-btn"
+                                  title="PDF Aprobado"
+                                >
+                                  <FaRegFilePdf style={{ color: "white" }} />
+                                </Button>
+                              </a>
+                            )}
+                            {regulation.pdf_process ? (
+                              <a target="_blank" href={regulation.pdf_process}>
+                                <Button
+                                  variant="primary"
+                                  className="pdf-btn"
+                                  title="PDF En Proceso"
+                                >
+                                  <FaRegFilePdf style={{ color: "white" }} />
+                                </Button>
+                              </a>
+                            ) : (
+                              // Botón inactivo para normativas que no tengan PDF
+                              <a target="_blank" disabled>
+                                <Button
+                                  title="PDF no disponible"
+                                  variant="secondary"
+                                  className="pdf-btn"
+                                  disabled
+                                >
+                                  <FaRegFilePdf style={{ color: "white" }} />
+                                </Button>
+                              </a>
+                            )}
+                          </div>
+                        )}
 
-        {(userRole === "admin" || userRole === "asesor") && (
-          <div>
-            <Button
-              title="Editar"
-              variant="warning"
-              className="edit-btn"
-              onClick={() => handleEditClick(regulation.id)}
-            >
-              <CiEdit />
-            </Button>
-          </div>
-        )}
+                        {(userRole === "admin" || userRole === "asesor") && (
+                          <div>
+                            <Button
+                              title="Editar"
+                              variant="warning"
+                              className="edit-btn"
+                              onClick={() => handleEditClick(regulation.id)}
+                            >
+                              <CiEdit />
+                            </Button>
+                          </div>
+                        )}
 
-        <div>
-          <Button
-            variant="info"
-            className="detail-btn"
-            title="Detalles"
-            onClick={() => handleDetailsClick(regulation.id)}
-          >
-            <TbListDetails />
-          </Button>
-        </div>
-      </div>
-    </td>
-  </tr>
-))}
-
+                        <div>
+                          <Button
+                            variant="info"
+                            className="detail-btn"
+                            title="Detalles"
+                            onClick={() => handleDetailsClick(regulation.id)}
+                          >
+                            <TbListDetails />
+                          </Button>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           )}
