9b89bfdc3ef175dde98358edf652f187a50164f4
CannataJulian <cannatajulian@gmail.com>
2025-03-19 20:28:04 -0300

    Prueba pdf

--- FrontEnd/src/components/adminPage/orderDay/ListOrderDay.jsx
+++ FrontEnd/src/components/adminPage/orderDay/ListOrderDay.jsx
@@ -1,4 +1,24 @@
 function ListOrderDay() {
+
+  const handleDownloadPDF = async () => {
+    try {
+      const response = await axios.get(API.GET_PDF, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+        responseType: "blob", // Importante para manejar archivos binarios
+      });
+  
+      // Crear una URL para el archivo PDF
+      const pdfBlob = new Blob([response.data], { type: "application/pdf" });
+      const pdfUrl = URL.createObjectURL(pdfBlob);
+  
+      // Abrir el PDF en una nueva pestaña
+      window.open(pdfUrl, "_blank");
+    } catch (error) {
+      console.error("Error al descargar el PDF:", error);
+    }
+  };
   return (
     <div className="page-form">
       <h1>Ordenes del día</h1>
