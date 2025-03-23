d98c7626a69d66268d85feade5b4f8e2c9fc94a1
CannataJulian <cannatajulian@gmail.com>
2025-03-23 15:21:38 -0300

    carga y visualizacion de contactos hecha

--- FrontEnd/src/components/cmPage/contactForm/contactForm.jsx
+++ FrontEnd/src/components/cmPage/contactForm/contactForm.jsx
@@ -1,37 +1,122 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import { Form, Button, Toast, ToastContainer } from "react-bootstrap";
+import axios from "axios";
 
 import API from "../../../config/apiConfig";
 function ContactForm() {
 
+  const [imgIzquierda, setImgIzquierda] = useState("");
+  const [imgDerecha, setImgDerecha] = useState("");
+
+  const [toastMessage, setToastMessage] = useState("");
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [showWarningToast, setShowWarningToast] = useState(false);
+  const [showErrorToast, setShowErrorToast] = useState(false);
+
+  const handleFileChange = (e, setImage) => {
+    const file = e.target.files[0];
+    if (file && (file.type === "image/png" || file.type === "image/jpeg")) {
+      setImage(file);
+    } else {
+      setToastMessage("Solo se permiten archivos PNG o JPG.");
+      setShowWarningToast(true);
+    }
+  };
+
   const handleSubmit = async (e) => {
-    e.prevent.default();
+    e.preventDefault();
 
+    if (!imgIzquierda && !imgDerecha) {
+      setToastMessage("Debe seleccionar al menos una imagen.");
+      setShowWarningToast(true);
+      return;
+    }
 
+    const formData = new FormData();
+    formData.append("image_left", imgIzquierda);
+    formData.append("image_right", imgDerecha);
+    console.log(formData);
+
+    try {
+      const response = await axios.post(API.UPLOAD_CONTACT, formData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (response.status === 200) {
+        setToastMessage("Imágenes cargadas exitosamente.");
+        setShowSuccessToast(true);
+      }
+
+    } catch (error) {
+      setToastMessage("Error al cargar las imágenes.");
+      setShowErrorToast(true);
+    }
   };
 
   return (
     <> 
       <div className="page-form">
         <div className="content-page-container">
-          <h2 className="internal-tittle">Imagenes del apartado contactos</h2>
+          <h1 className="internal-title">Imagenes del apartado contactos</h1>
           <div className="content-form">
-            <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3">
-                <Form.Label>Imagen izquierda</Form.Label>
-                <Form.Control type="file"/>
+                <Form.Label>Imagen izquierda:</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => handleFileChange(e, setImgIzquierda)}
+                />
               </Form.Group>
-              <Form.Group>
-                <Form.Label>Imagen derecha</Form.Label>
-                <Form.Control type="file"/>
+              <Form.Group className="mb-3">
+                <Form.Label>Imagen derecha:</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => handleFileChange(e, setImgDerecha)}
+                />
               </Form.Group>
-              <Button>
-                Cargar imagenes
-              </Button>
+              <Button type="submit">Cargar imágenes</Button>
             </Form>
           </div>
         </div>
       </div>
+
+            {/* Toasts para feedback */}
+            <ToastContainer position="top-end" className="p-3">
+        <Toast
+          bg="danger"
+          onClose={() => setShowErrorToast(false)}
+          show={showErrorToast}
+          delay={3000}
+          autohide
+        >
+          <Toast.Body className="text-white">{toastMessage}</Toast.Body>
+        </Toast>
+      </ToastContainer>
+      <ToastContainer position="top-end" className="p-3">
+        <Toast
+          bg="warning"
+          onClose={() => setShowWarningToast(false)}
+          show={showWarningToast}
+          delay={3000}
+          autohide
+        >
+          <Toast.Body className="text-white">{toastMessage}</Toast.Body>
+        </Toast>
+      </ToastContainer>
+      <ToastContainer position="top-end" className="p-3">
+        <Toast
+          bg="success"
+          onClose={() => setShowSuccessToast(false)}
+          show={showSuccessToast}
+          delay={3000}
+          autohide
+        >
+          <Toast.Body className="text-white">{toastMessage}</Toast.Body>
+        </Toast>
+      </ToastContainer>
         
     </>
 
--- FrontEnd/src/components/contact/Contact.jsx
+++ FrontEnd/src/components/contact/Contact.jsx
@@ -1,24 +1,60 @@
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import API from "../../config/apiConfig";
 import "./contact.css";
 
 function Contact() {
+  const [images, setImages] = useState({ image_left: "", image_right: "" });
+
+  const getContacts = async () => {
+    try {
+      let headersList = {
+        "Content-Type": "application/json",
+      };
+
+      let reqOptions = {
+        url: API.GET_CONTACT,
+        method: "GET",
+        headers: headersList,
+      };
+
+      const response = await axios.request(reqOptions);
+
+      if (response.status === 200) {
+        setImages(response.data[0]);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log("Error al obtener las imagenes.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <>
-    <div className="news-container">
-    <div className="title-news">
-        <p>Contacto</p>
-      </div>
-      <div className="container-forms">
-        <div className="form">
-          <img src="/assets/tel.png" alt="" />
+      <div className="news-container">
+        <div className="title-news">
+          <p>Contacto</p>
         </div>
-        <div className="form">
-          <img src="/assets/contacto.png" alt="" />
+        <div className="container-forms">
+          {images.image_left && images.image_right ? (
+            <>
+              <div className="form">
+                <img src={images.image_left} alt="Imagen Izquierda" />
+              </div>
+              <div className="form">
+                <img src={images.image_right} alt="Imagen derecha" />
+              </div>
+            </>
+          ) : (
+            <p>Cargando imagenes...</p>
+          )}
         </div>
       </div>
-
-    </div>
-      
     </>
   );
 }
