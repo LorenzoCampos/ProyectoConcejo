f585cd11e780cb16b2ca69bd8e1ceaea5f056158
LoloCampos <lolocampos544@gmail.com>
2025-03-24 15:28:13 -0300

    Minor correction in cors.php.

--- backend-api/config/cors.php
+++ backend-api/config/cors.php
@@ -25,7 +25,7 @@ return [
 
     'paths' => ['*'],  // Asegúrate de incluir todos los endpoints necesarios
     'allowed_methods' => ['*'], // Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
-    'allowed_origins' => ['https://concejoarroyoseco.duckdns.org', 'https://4rkgr627-5173.brs.devtunnels.ms'], // Permite el origen de tu frontend
+    'allowed_origins' => ['*'], // Permite el origen de tu frontend
     'allowed_headers' => ['*'],  // Permite todos los encabezados
     'exposed_headers' => [],
     'max_age' => 0,
